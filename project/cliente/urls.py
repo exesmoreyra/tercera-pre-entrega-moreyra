5d560118e0a8255301a4f04b187323418b57344f
exequiel.moreyra <exesmoreyra@gmail.com>
2023-12-13 23:49:12 -0300

    add base.html

--- project/cliente/urls.py
+++ project/cliente/urls.py
@@ -3,6 +3,8 @@ from django.urls import path
 from . import views 
 #se usan importaciones relativas con un .
 
+app_name = "cliente"
+
 urlpatterns = [
-    path("", views.home),
+    path("", views.home, name="index"),
 ]
--- project/core/urls.py
+++ project/core/urls.py
@@ -2,7 +2,8 @@ from django.urls import path
 
 from . import views 
 #se usan importaciones relativas con un .
+app_name = "core"
 
 urlpatterns = [
-    path("", views.home),
+    path("", views.home, name="index"),
 ]
