b98573afb65616f4c8d267817633562380cc01a9
exequiel.moreyra <exesmoreyra@gmail.com>
2023-12-14 00:09:15 -0300

    add sqlite

--- project/cliente/models.py
+++ project/cliente/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+class Pais(models.Model):
+    nombre = models.CharField(max_length=100)
+
+class Cliente(models.Model):
+    nombre = models.CharField(max_length=100)
+    apellido = models.CharField(max_length=100)
+    nacimiento = models.DateField(null=True, blank=True)
+    pais_origen = models.ForeignKey(Pais, on_delete=models.SET_NULL, null=True, blank=True)    
